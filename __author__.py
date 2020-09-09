0023dbecc499912ead8776a3b6066e2e4eda1cbf
QAQmander <salamander@buaa.edu.cn>
2020-09-09 11:26:07 +0800

    update testers

--- __author__.py
+++ __author__.py
@@ -3,4 +3,9 @@ __author__ = [
 ]
 
 __close_alpha_participant__ = [
+    ('Pan Haowen', 2019),
+    ('Huang Xiang', 2019),
+    ('Hu Ruinan', 2019),
+    ('Fu Yunhao', 2019),
+    ('Fang Yijia', 2019),
 ]
