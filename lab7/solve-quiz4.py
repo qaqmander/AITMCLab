4827607613f77a2a0b10faca986b459883fc0a27
qaqmander <qaqmander@gmail.com>
2020-12-31 22:24:06 +0800

    update solve-quiz4.py: fix bug, support (ps[i], rs[i])

--- lab7/solve-quiz4.py
+++ lab7/solve-quiz4.py
@@ -8,26 +8,45 @@ with open('enc', 'rb') as fr:
     y = int(fr.readline().strip().split()[-1])
 
 ps = []
+rs = []
 gs = []
 ys = []
 N = n - 1
 for p in range(2, 1 << 12):
     if N % p == 0:
         ps.append(p)
-        gs.append(pow(g, (n - 1) / p, n))
-        ys.append(pow(y, (n - 1) / p, n))
-        N //= p
+        r = 0
+        while N % p == 0:
+            r += 1 
+            N //= p
+        rs.append(r)
+        gs.append(pow(g, (n - 1) / (p ** r), n))
+        ys.append(pow(y, (n - 1) / (p ** r), n))
+
+print ps
+print rs
 
 xs = []
-for i in range(len(ps)):
-    now = 1
-    for x in range(ps[i] + 1):
-        if now == ys[i]:
-            xs.append(x)
-            break
-        now = now * gs[i] % n
+for idx in range(len(ps)):
+    p, r = ps[idx], rs[idx]
+    g, y = gs[idx], ys[idx]
+    a = [0] * r
+    now_x = 0
+    for i in range(r):
+        now_g = pow(g, pow(p, r - 1 - i), n)
+        now_y = pow(y, pow(p, r - 1 - i), n)
+        now = pow(now_g, now_x, n)
+        for now_a in range(p + 1):
+            if now == now_y:
+                a[i] = now_a
+                break
+            now = now * pow(now_g, pow(p, i), n) % n
+        now_x += a[i] * pow(p, i)
+    xs.append(now_x)
 print xs
-print ps
+
+for i in range(len(ps)):
+    ps[i] = pow(ps[i], rs[i])
 
 Ms = []
 m = 1
